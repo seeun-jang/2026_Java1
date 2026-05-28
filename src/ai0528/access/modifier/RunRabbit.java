7de7b2a6bacbae959c04c5571d72772e561e5097
seeun-jang <jangseeun108@gmail.com>
2026-05-28 11:47:51 +0900

    Java 7장 클래스와 객체 - 접근제한자

---
+++ src/ai0528/access/modifier/RunRabbit.java
@@ -0,0 +1,16 @@
+package ai0528.access.modifier;
+
+public class RunRabbit {
+    public static void main(String[] args) {
+        Rabbit rabbit = new Rabbit();
+
+//        rabbit.setShape("동그라미");
+        rabbit.shape = "삼각형";
+        rabbit.printInfo();
+//        rabbit.setLocation(50, 80);
+        rabbit.x = 70;
+        rabbit.y = 100;
+        rabbit.printInfo();
+    }
+}
+
